33bcbe6f155f512b338a79a517fac995195d5cff
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-01-15 05:59:18 +0530

    Announcement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Annnouncement } from "../component/Annnouncement";
 import Navbar from "../component/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Annnouncement />
       <Navbar />
     </div>
   );
