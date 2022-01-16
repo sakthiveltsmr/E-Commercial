cb62dd28bc5636b72cc2d97f7b9e1aa9174f614b
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-01-16 16:18:23 +0530

    slider

---
+++ src/component/Slider.jsx
@@ -0,0 +1,88 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  //   background-color: coral;
+  position: relative;
+`;
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+`;
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+const Img = styled.img`
+  height: 80%;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+const Tittle = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+export const Slider = () => {
+  return (
+    <Container>
+      <Arrow direction="left">
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Img src="https://marketing4ecommerce.net/wp-content/uploads/2015/07/experiencia-de-compra3-1-759x500.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Tittle>SUMMER OFFER!</Tittle>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS
+            </Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Annnouncement } from "../component/Annnouncement";
 import Navbar from "../component/Navbar";
+import { Slider } from "../component/Slider";
 
 export default function Home() {
   return (
     <div>
       <Annnouncement />
       <Navbar />
+      <Slider />
     </div>
   );
 }
