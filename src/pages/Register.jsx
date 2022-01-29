52eb4bd9892d76f82409e4f3802cf79fea37adcb
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-01-29 15:06:01 +0530

    Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -61,12 +61,12 @@ const Register = () => {
       <Wrapper>
         <Title>CREATE AN ACCOUNT</Title>
         <Form>
-          <Input placeholder="name" />
-          <Input placeholder="last name" />
-          <Input placeholder="username" />
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Input placeholder="confirm password" />
+          <Input placeholder="first name" required />
+          <Input placeholder="last name" required />
+          <Input placeholder="username" required />
+          <Input placeholder="email" required />
+          <Input placeholder="password" required />
+          <Input placeholder="confirm password" required />
           <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
